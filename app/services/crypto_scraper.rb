0cc04f0d5678b412e21e50c63bcd183792278cff
Ron Parnaso <roland.parnaso@gmail.com>
2018-06-10 19:03:32 -0700

    hotfix stake.

--- app/services/crypto_scraper.rb
+++ app/services/crypto_scraper.rb
@@ -29,7 +29,7 @@ class CryptoScraper
         crypto.node_price  = browser.find_elements(tag_name: 'mnp-data-box')[2].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
         crypto.nodes       = browser.find_elements(tag_name: 'mnp-data-box')[3].text&.split(/\n/).first.gsub(/\D/,'').to_i
         crypto.price       = browser.find_elements(tag_name: 'mnp-data-box')[6].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
-        crypto.stake       = browser.find_elements(tag_name: 'mnp-data-box')[5].text&.split(/\s/).first.gsub(/\D/,'').to_i
+        crypto.stake       = browser.find_elements(tag_name: 'mnp-data-box')[4].text&.split(/\s/).first.gsub(/\D/,'').to_i
         crypto.url         = browser.find_elements(tag_name: 'a').find{ |a| a.attribute('title') == 'WebSite' }.attribute('href').split("r=")[1]
         crypto.save
       rescue => error
@@ -51,7 +51,7 @@ class CryptoScraper
         crypto.node_price  = browser.wd.find_elements(tag_name: 'mnp-data-box')[2].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
         crypto.nodes       = browser.wd.find_elements(tag_name: 'mnp-data-box')[3].text&.split(/\n/).first.gsub(/\D/,'').to_i
         crypto.price       = browser.wd.find_elements(tag_name: 'mnp-data-box')[6].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
-        crypto.stake       = browser.wd.find_elements(tag_name: 'mnp-data-box')[5].text&.split(/\s/).first.gsub(/\D/,'').to_i
+        crypto.stake       = browser.wd.find_elements(tag_name: 'mnp-data-box')[4].text&.split(/\s/).first.gsub(/\D/,'').to_i
         crypto.url         = browser.a(title: 'WebSite').href.split("r=")[1]
         crypto.save
       rescue => error
