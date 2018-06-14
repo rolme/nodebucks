7bda9a4784e8d4b4cfcfd4a4b2c0b0a814fa0799
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-06-14 10:27:00 +0400

    Need to do some correction according to upcoming design of mobile view

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -31,10 +31,10 @@ export default class Home extends Component {
         </div>
         <div className="homeAboutContainer">
           <div className="contentContainer flex-wrap">
-            <Col xl={{size: 5, offset: 0}} lg={{size: 5, offset: 0}} md={{size: 6, offset: 3}} sm={{size: 6, offset: 3}} xs={{size: 6, offset: 3}} className="d-flex justify-content-center">
+            <Col xl={{ size: 5, offset: 0 }} lg={{ size: 5, offset: 0 }} md={{ size: 6, offset: 3 }} sm={{ size: 6, offset: 3 }} xs={{ size: 6, offset: 3 }} className="d-flex justify-content-center">
               <img src="/assets/images/illustration.png" alt='Illustration'/>
             </Col>
-            <Col xl={{size: 7, offset: 0}} lg={{size: 7, offset: 0}} md={{size: 8, offset: 2}} sm={{size: 10, offset: 1}} xs={{size: 10, offset: 1}} className="homeAboutTextPartContainer ">
+            <Col xl={{ size: 7, offset: 0 }} lg={{ size: 7, offset: 0 }} md={{ size: 8, offset: 2 }} sm={{ size: 10, offset: 1 }} xs={{ size: 10, offset: 1 }} className="homeAboutTextPartContainer ">
               <h1>Blockchain is the Future</h1>
               <p>The blockchain is disrupting nearly every major industry and is reshaping how businesses think and operate. Masternodes are the cornerstone of a new type of blockchain, acting as the foundation for many popular cryptocurrencies.</p>
               <p> Now you can own and operate your very own masternode and collect rewards in exchange for the work your masternode performs on the blockchain, confirming and verifying transactions.</p>
@@ -43,6 +43,58 @@ export default class Home extends Component {
         </div>
         <Masternodes/>
         <Testimonials/>
+        <div className="homeWhatIsMasternodeContainer bg-white">
+          <div className="contentContainer flex-wrap">
+            <h1 className="homeWhatIsMasternodeHeader">What is masternode?</h1>
+            <div className="homeWhatIsMasternodeContentPartContainer">
+              <Col className="d-flex align-items-center">
+                <Col xl={{ size: 6, offset: 0 }} lg={{ size: 5, offset: 0 }}>
+                  <p className="homeWhatIsMasternodeText">A blockchain requires a network of computers called nodes to confirm and record transactions. As the number of nodes grows, the power and security of the blockchain network increases. </p>
+                </Col>
+                <Col xl={{ size: 5, offset: 1 }} lg={{ size: 6, offset: 1 }}>
+                  <img src="/assets/images/whatIsMasternode1.png" alt="What is Masternode?"/>
+                </Col>
+              </Col>
+              <Col>
+                <Col xl={{ size: 6, offset: 2 }} lg={{ size: 6, offset: 2 }} className="homeWhatIsMasternodeFirstDotesImageContainer">
+                  <img src="/assets/images/whatIsMasternodeDotes1.png" alt="What is Masternode?"/>
+                </Col>
+              </Col>
+              <Col className="d-flex align-items-center">
+                <Col xl={{ size: 5, offset: 0 }} className="homeWhatIsMasternodeSecondImageContainer">
+                  <img src="/assets/images/whatIsMasternode2.png" alt="What is Masternode?"/>
+                </Col>
+                <Col xl={{ size: 6, offset: 1 }}>
+                  <p className="homeWhatIsMasternodeText">A masternode is a special node on the blockchain that has enhanced capabilities and voting rights within proof-of-stake (PoS) coins. By owning a masternode, the masternode operator is rewarded by the network for the time and energy of operating the node.
+                    <br/><br/>This involves keeping the server online 24x7 as well as occasionally upgrading the software.</p>
+                </Col>
+              </Col>
+              <Col>
+                <Col xl={{ size: 6, offset: 2 }} className="homeWhatIsMasternodeSecondDotesImageContainer">
+                  <img src="/assets/images/whatIsMasternodeDotes2.png" alt="What is Masternode?"/>
+                </Col>
+              </Col>
+              <Col className="d-flex align-items-center">
+                <Col xl={{ size: 6, offset: 0 }}>
+                  <p className="homeWhatIsMasternodeText">Nodebucks makes it easy for you to own your own masternodes without the technical expertise involved in setting it up and maintaining it. </p>
+                </Col>
+                <Col xl={{ size: 5, offset: 1 }}>
+                  <img src="/assets/images/whatIsMasternode3.png" alt="What is Masternode?"/>
+                </Col>
+              </Col>
+            </div>
+          </div>
+        </div>
+        <div className="homeFooterContainer">
+          <div className="contentContainer d-flex align-items-center flex-wrap">
+            <Col xl={{size: 6, offset: 1}} lg={{size: 6, offset: 1}} md={{size: 8, offset: 2}} sm={{size: 8, offset: 2}} xs={{size: 10, offset: 1}}>
+              <p className="homeFooterText mb-0">Owning a <span>masternode</span> has never been easier! </p>
+            </Col>
+            <Col xl={{size: 4, offset: 1}} lg={{size: 4, offset: 1}} md={{size: 6, offset: 3}} sm={{size: 6, offset: 3}} xs={{size: 10, offset: 1}} className="d-flex justify-content-center mt-xl-0 mt-lg-0 mt-md-3 mt-sm-4 mt-xs-5">
+              <button className="homeFooterButton"><img src="/assets/images/masternode.png" alt='masternode'/>Setup a Masternode</button>
+            </Col>
+          </div>
+        </div>
       </div>
     )
   }
--- client/src/containers/home/testimonials.js
+++ client/src/containers/home/testimonials.js
@@ -22,13 +22,13 @@ export default class Testimonials extends Component {
           <h1 className="homeTestimonialsHeader">Testimonials</h1>
           <Slider {...settings}>
             <div>
-              <div className="homeTestimonialsSlideContentContainer">
-                <Col xl={{size: 3, offset: 1}} className='homeTestimonialAvatarPartContainer'>
+              <div className="homeTestimonialsSlideContentContainer flex-wrap">
+                <Col xl={{size: 3, offset: 1}} lg={{size: 3, offset: 1}}  md={{size: 3, offset: 1}}  sm={{size: 6, offset: 3}} xs={{size: 6, offset: 3}} className='homeTestimonialAvatarPartContainer'>
                   <img src="/assets/images/testimonialAvatar.png" alt="Testimonial" className="testimonialAvatar"/>
                   <p className="testimonialName">Scott Bennet</p>
                   <p className="testimonialPosition">Police Officer</p>
                 </Col>
-                <Col xl={{size: 7, offset: 1}}>
+                <Col xl={{size: 7, offset: 1}} lg={{size: 7, offset: 1}}  md={{size: 7, offset: 1}}  sm={{size: 10, offset: 1}} xs={{size: 12, offset: 0}}>
                   <img src="/assets/images/openQuotes.png" alt="Quotes"/>
                   <p className="testimonialMessageText">I always wanted to own a piece of the blockchain and a masternode seemed liked a great way to do it, but I didn’t have the technical background to build one or the time to maintain it. Nodebucks makes it easy to run your own masternodes and I love the clean intuitive interface, the transparency, and reporting that it provides.</p>
                   <img src="/assets/images/closeQuotes.png" alt="Quotes"/>
