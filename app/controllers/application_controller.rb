c12add24f26be58c797ea605d26d2c81c9ffdf05
Roland Parnaso <Roland Parnaso>
2018-06-14 15:58:30 -0700

    Add API to login and get a JWT.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  include ExceptionHandler
+
+  def index
+    render file: 'public/index.html', content_type: 'text/html'
+  end
+
+private
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
+
+  def authenticate_admin_request
+    user = AuthorizeApiRequest.call(request.headers).result
+    @current_user = user if user.admin?
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
 end
