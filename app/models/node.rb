9e3c7e04f7e1a2875fbd8adc477811e58b850584
Ron Parnaso <roland.parnaso@gmail.com>
2018-06-15 14:24:35 -0700

    WIP

---
+++ app/models/node.rb
@@ -0,0 +1,4 @@
+class Node < ApplicationRecord
+  belongs_to :user
+  belongs_to :crypto
+end
---
+++ db/migrate/20180615212051_create_nodes.rb
@@ -0,0 +1,21 @@
+class CreateNodes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :nodes do |t|
+      t.references :user, foreign_key: true
+      t.references :crypto, foreign_key: true
+      t.string :status, default: :new
+      t.string :ip
+      t.decimal :cost
+      t.integer :created_by_admin_id
+      t.datetime :online_at
+      t.datetime :sold_at
+      t.string :version
+      t.datetime :last_upgraded_at
+      t.string :vps_provider
+      t.string :vps_url
+      t.decimal :vps_monthly_cost
+
+      t.timestamps
+    end
+  end
+end
