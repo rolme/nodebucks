1e803c66cf247fb477b2199b460ad8ee63b0932a
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-06-18 18:23:28 +0400

    WIP - UI finished

--- client/src/components/elements/inputField.js
+++ client/src/components/elements/inputField.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { FormGroup, Label, Input, FormFeedback, FormText, Col } from 'reactstrap';
+import React, { Component } from 'react'
+import { FormGroup, Label, Input, InputGroupAddon, InputGroupText, FormText, Col } from 'reactstrap'
 
 import './index.css'
 
@@ -11,19 +11,27 @@ export default class InputField extends Component {
   }
 
   render() {
-    const { label, name, type, value, message, error } = this.props
+    const { label, name, type, value, message, error, addonIcon } = this.props
     let inputFieldContainerClassName = 'inputFieldContainer form-control'
-    if(!!value){
+    if ( !!value ) {
       inputFieldContainerClassName += ' inputFieldContainerWithValue'
     }
-    if(error){
+    if ( error ) {
       inputFieldContainerClassName += ' inputFieldContainerWithError'
+    }
+    if ( !!addonIcon ) {
+      inputFieldContainerClassName += ' input-group withAddonInputFieldGroup'
     }
     return (
       <FormGroup className="w-100">
         <Col className={inputFieldContainerClassName}>
           {!!value && <Label for={name}>{label}</Label>}
           <Input type={type} name={name} id={name} value={value} placeholder={label} onChange={(event) => this.props.handleFieldValueChange(event.target.value, name)}/>
+          {!!addonIcon &&
+          <InputGroupAddon addonType="append">
+            <InputGroupText><img src={addonIcon} alt='psw' onClick={() => this.props.onAddonClick('showPassword')} className={!!this.props.onAddonClick ? "inputFieldClickableAddon" : ""}/></InputGroupText>
+          </InputGroupAddon>
+          }
         </Col>
         <FormText className={error ? 'inputFieldErrorMessage' : 'inputFieldMessage'}>{message}</FormText>
       </FormGroup>
--- client/src/containers/authenticate/signUp.js
+++ client/src/containers/authenticate/signUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-
-import InputField from '../../components/inputField'
-import { Container, Col } from 'reactstrap'
+import InputField from '../../components/elements/inputField'
+import SelectField from '../../components/elements/selectField'
+import { Container, Col, Button } from 'reactstrap'
 import './index.css'
 
 export default class SignUp extends Component {
@@ -10,25 +10,109 @@ export default class SignUp extends Component {
 
     this.state = {
       firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      zip: '',
+      city: '',
+      state: '',
+      address: '',
+      country: '',
+      showPassword: false,
+      showConfirmPassword: false,
       stepNumber: 1,
     }
     this.handleFieldValueChange = this.handleFieldValueChange.bind(this)
+    this.onAddonClick = this.onAddonClick.bind(this)
+    this.stepOneValidation = this.stepOneValidation.bind(this)
+    this.stepTwoValidation = this.stepTwoValidation.bind(this)
   }
 
   handleFieldValueChange(newValue, name) {
-    this.setState({
-      [name]: newValue,
-    })
+    this.setState({ [name]: newValue, })
   }
 
+  onAddonClick(name) {
+    this.setState({ [name]: !this.state[ name ] })
+  }
+
+  stepOneValidation() {
+    this.setState({ stepNumber: this.state.stepNumber + 1 })
+  }
+
+  stepTwoValidation() {
+  }
+
+
   render() {
-    const { stepNumber, firstName } = this.state
+    const countryOptions = [
+      { "value": "Afghanistan", "label": "Afghanistan" }, { "value": "Albania", "label": "Albania" }, { "value": "Algeria", "label": "Algeria" }, { "value": "Andorra", "label": "Andorra" }, { "value": "Angola", "label": "Angola" }, { "value": "Anguilla", "label": "Anguilla" }, { "value": "Antigua &amp; Barbuda", "label": "Antigua &amp; Barbuda" }, { "value": "Argentina", "label": "Argentina" }, { "value": "Armenia", "label": "Armenia" }, {
+        "value": "Aruba",
+        "label": "Aruba"
+      }, { "value": "Australia", "label": "Australia" }, { "value": "Austria", "label": "Austria" }, { "value": "Azerbaijan", "label": "Azerbaijan" }, { "value": "Bahamas", "label": "Bahamas" }, { "value": "Bahrain", "label": "Bahrain" }, { "value": "Bangladesh", "label": "Bangladesh" }, { "value": "Barbados", "label": "Barbados" }, { "value": "Belarus", "label": "Belarus" }, { "value": "Belgium", "label": "Belgium" }, { "value": "Belize", "label": "Belize" }, {
+        "value": "Benin",
+        "label": "Benin"
+      }, { "value": "Bermuda", "label": "Bermuda" }, { "value": "Bhutan", "label": "Bhutan" }, { "value": "Bolivia", "label": "Bolivia" }, { "value": "Bosnia &amp; Herzegovina", "label": "Bosnia &amp; Herzegovina" }, { "value": "Botswana", "label": "Botswana" }, { "value": "Brazil", "label": "Brazil" }, { "value": "British Virgin Islands", "label": "British Virgin Islands" }, { "value": "Brunei", "label": "Brunei" }, { "value": "Bulgaria", "label": "Bulgaria" }, {
+        "value": "Burkina Faso",
+        "label": "Burkina Faso"
+      }, { "value": "Burundi", "label": "Burundi" }, { "value": "Cambodia", "label": "Cambodia" }, { "value": "Cameroon", "label": "Cameroon" }, { "value": "Canada", "label": "Canada" }, { "value": "Cape Verde", "label": "Cape Verde" }, { "value": "Cayman Islands", "label": "Cayman Islands" }, { "value": "Chad", "label": "Chad" }, { "value": "Chile", "label": "Chile" }, { "value": "China", "label": "China" }, { "value": "Colombia", "label": "Colombia" }, {
+        "value": "Congo",
+        "label": "Congo"
+      }, { "value": "Cook Islands", "label": "Cook Islands" }, { "value": "Costa Rica", "label": "Costa Rica" }, { "value": "Cote D Ivoire", "label": "Cote D Ivoire" }, { "value": "Croatia", "label": "Croatia" }, { "value": "Cruise Ship", "label": "Cruise Ship" }, { "value": "Cuba", "label": "Cuba" }, { "value": "Cyprus", "label": "Cyprus" }, { "value": "Czech Republic", "label": "Czech Republic" }, { "value": "Denmark", "label": "Denmark" }, {
+        "value": "Djibouti",
+        "label": "Djibouti"
+      }, { "value": "Dominica", "label": "Dominica" }, { "value": "Dominican Republic", "label": "Dominican Republic" }, { "value": "Ecuador", "label": "Ecuador" }, { "value": "Egypt", "label": "Egypt" }, { "value": "El Salvador", "label": "El Salvador" }, { "value": "Equatorial Guinea", "label": "Equatorial Guinea" }, { "value": "Estonia", "label": "Estonia" }, { "value": "Ethiopia", "label": "Ethiopia" }, { "value": "Falkland Islands", "label": "Falkland Islands" }, {
+        "value": "Faroe Islands",
+        "label": "Faroe Islands"
+      }, { "value": "Fiji", "label": "Fiji" }, { "value": "Finland", "label": "Finland" }, { "value": "France", "label": "France" }, { "value": "French Polynesia", "label": "French Polynesia" }, { "value": "French West Indies", "label": "French West Indies" }, { "value": "Gabon", "label": "Gabon" }, { "value": "Gambia", "label": "Gambia" }, { "value": "Georgia", "label": "Georgia" }, { "value": "Germany", "label": "Germany" }, { "value": "Ghana", "label": "Ghana" }, {
+        "value": "Gibraltar",
+        "label": "Gibraltar"
+      }, { "value": "Greece", "label": "Greece" }, { "value": "Greenland", "label": "Greenland" }, { "value": "Grenada", "label": "Grenada" }, { "value": "Guam", "label": "Guam" }, { "value": "Guatemala", "label": "Guatemala" }, { "value": "Guernsey", "label": "Guernsey" }, { "value": "Guinea", "label": "Guinea" }, { "value": "Guinea Bissau", "label": "Guinea Bissau" }, { "value": "Guyana", "label": "Guyana" }, { "value": "Haiti", "label": "Haiti" }, {
+        "value": "Honduras",
+        "label": "Honduras"
+      }, { "value": "Hong Kong", "label": "Hong Kong" }, { "value": "Hungary", "label": "Hungary" }, { "value": "Iceland", "label": "Iceland" }, { "value": "India", "label": "India" }, { "value": "Indonesia", "label": "Indonesia" }, { "value": "Iran", "label": "Iran" }, { "value": "Iraq", "label": "Iraq" }, { "value": "Ireland", "label": "Ireland" }, { "value": "Isle of Man", "label": "Isle of Man" }, { "value": "Israel", "label": "Israel" }, {
+        "value": "Italy",
+        "label": "Italy"
+      }, { "value": "Jamaica", "label": "Jamaica" }, { "value": "Japan", "label": "Japan" }, { "value": "Jersey", "label": "Jersey" }, { "value": "Jordan", "label": "Jordan" }, { "value": "Kazakhstan", "label": "Kazakhstan" }, { "value": "Kenya", "label": "Kenya" }, { "value": "Kuwait", "label": "Kuwait" }, { "value": "Kyrgyz Republic", "label": "Kyrgyz Republic" }, { "value": "Laos", "label": "Laos" }, { "value": "Latvia", "label": "Latvia" }, {
+        "value": "Lebanon",
+        "label": "Lebanon"
+      }, { "value": "Lesotho", "label": "Lesotho" }, { "value": "Liberia", "label": "Liberia" }, { "value": "Libya", "label": "Libya" }, { "value": "Liechtenstein", "label": "Liechtenstein" }, { "value": "Lithuania", "label": "Lithuania" }, { "value": "Luxembourg", "label": "Luxembourg" }, { "value": "Macau", "label": "Macau" }, { "value": "Macedonia", "label": "Macedonia" }, { "value": "Madagascar", "label": "Madagascar" }, { "value": "Malawi", "label": "Malawi" }, {
+        "value": "Malaysia",
+        "label": "Malaysia"
+      }, { "value": "Maldives", "label": "Maldives" }, { "value": "Mali", "label": "Mali" }, { "value": "Malta", "label": "Malta" }, { "value": "Mauritania", "label": "Mauritania" }, { "value": "Mauritius", "label": "Mauritius" }, { "value": "Mexico", "label": "Mexico" }, { "value": "Moldova", "label": "Moldova" }, { "value": "Monaco", "label": "Monaco" }, { "value": "Mongolia", "label": "Mongolia" }, { "value": "Montenegro", "label": "Montenegro" }, {
+        "value": "Montserrat",
+        "label": "Montserrat"
+      }, { "value": "Morocco", "label": "Morocco" }, { "value": "Mozambique", "label": "Mozambique" }, { "value": "Namibia", "label": "Namibia" }, { "value": "Nepal", "label": "Nepal" }, { "value": "Netherlands", "label": "Netherlands" }, { "value": "Netherlands Antilles", "label": "Netherlands Antilles" }, { "value": "New Caledonia", "label": "New Caledonia" }, { "value": "New Zealand", "label": "New Zealand" }, { "value": "Nicaragua", "label": "Nicaragua" }, {
+        "value": "Niger",
+        "label": "Niger"
+      }, { "value": "Nigeria", "label": "Nigeria" }, { "value": "Norway", "label": "Norway" }, { "value": "Oman", "label": "Oman" }, { "value": "Pakistan", "label": "Pakistan" }, { "value": "Palestine", "label": "Palestine" }, { "value": "Panama", "label": "Panama" }, { "value": "Papua New Guinea", "label": "Papua New Guinea" }, { "value": "Paraguay", "label": "Paraguay" }, { "value": "Peru", "label": "Peru" }, { "value": "Philippines", "label": "Philippines" }, {
+        "value": "Poland",
+        "label": "Poland"
+      }, { "value": "Portugal", "label": "Portugal" }, { "value": "Puerto Rico", "label": "Puerto Rico" }, { "value": "Qatar", "label": "Qatar" }, { "value": "Reunion", "label": "Reunion" }, { "value": "Romania", "label": "Romania" }, { "value": "Russia", "label": "Russia" }, { "value": "Rwanda", "label": "Rwanda" }, { "value": "Saint Pierre &amp; Miquelon", "label": "Saint Pierre &amp; Miquelon" }, { "value": "Samoa", "label": "Samoa" }, {
+        "value": "San Marino",
+        "label": "San Marino"
+      }, { "value": "Satellite", "label": "Satellite" }, { "value": "Saudi Arabia", "label": "Saudi Arabia" }, { "value": "Senegal", "label": "Senegal" }, { "value": "Serbia", "label": "Serbia" }, { "value": "Seychelles", "label": "Seychelles" }, { "value": "Sierra Leone", "label": "Sierra Leone" }, { "value": "Singapore", "label": "Singapore" }, { "value": "Slovakia", "label": "Slovakia" }, { "value": "Slovenia", "label": "Slovenia" }, {
+        "value": "South Africa",
+        "label": "South Africa"
+      }, { "value": "South Korea", "label": "South Korea" }, { "value": "Spain", "label": "Spain" }, { "value": "Sri Lanka", "label": "Sri Lanka" }, { "value": "St Kitts &amp; Nevis", "label": "St Kitts &amp; Nevis" }, { "value": "St Lucia", "label": "St Lucia" }, { "value": "St Vincent", "label": "St Vincent" }, { "value": "St. Lucia", "label": "St. Lucia" }, { "value": "Sudan", "label": "Sudan" }, { "value": "Suriname", "label": "Suriname" }, {
+        "value": "Swaziland",
+        "label": "Swaziland"
+      }, { "value": "Sweden", "label": "Sweden" }, { "value": "Switzerland", "label": "Switzerland" }, { "value": "Syria", "label": "Syria" }, { "value": "Taiwan", "label": "Taiwan" }, { "value": "Tajikistan", "label": "Tajikistan" }, { "value": "Tanzania", "label": "Tanzania" }, { "value": "Thailand", "label": "Thailand" }, { "value": "Timor L'Este", "label": "Timor L'Este" }, { "value": "Togo", "label": "Togo" }, { "value": "Tonga", "label": "Tonga" }, {
+        "value": "Trinidad &amp; Tobago",
+        "label": "Trinidad &amp; Tobago"
+      }, { "value": "Tunisia", "label": "Tunisia" }, { "value": "Turkey", "label": "Turkey" }, { "value": "Turkmenistan", "label": "Turkmenistan" }, { "value": "Turks &amp; Caicos", "label": "Turks &amp; Caicos" }, { "value": "Uganda", "label": "Uganda" }, { "value": "Ukraine", "label": "Ukraine" }, { "value": "United Arab Emirates", "label": "United Arab Emirates" }, { "value": "United Kingdom", "label": "United Kingdom" }, {
+        "value": "United States",
+        "label": "United States"
+      }, { "value": "United States Minor Outlying Islands", "label": "United States Minor Outlying Islands" }, { "value": "Uruguay", "label": "Uruguay" }, { "value": "Uzbekistan", "label": "Uzbekistan" }, { "value": "Venezuela", "label": "Venezuela" }, { "value": "Vietnam", "label": "Vietnam" }, { "value": "Virgin Islands (US)", "label": "Virgin Islands (US)" }, { "value": "Yemen", "label": "Yemen" }, { "value": "Zambia", "label": "Zambia" }, { "value": "Zimbabwe", "label": "Zimbabwe" }
+    ]
+    const { stepNumber, firstName, lastName, email, password, confirmPassword, zip, city, state, address, country, showPassword, showConfirmPassword } = this.state
     return (
-      <Container fluid className="bg-white">
+      <Container fluid className="bg-white signUpPageContainer">
         <div className="contentContainer d-flex justify-content-center">
           <Col className="signUpContainer">
             {stepNumber === 1 &&
-            <Col xl={{size: 4, offset: 4}} className="justify-content-center d-flex flex-column align-items-center">
+            <Col xl={{ size: 4, offset: 4 }} className="justify-content-center d-flex flex-column align-items-center">
               <img src="/assets/images/signUpFirstStepIcon.png" alt="Step 1"/>
               <p className="signUpStepNumber">Step 1 of 2</p>
               <h2 className="signUpHeader">Let's get started.</h2>
@@ -40,6 +124,95 @@ export default class SignUp extends Component {
                           error={false}
                           handleFieldValueChange={this.handleFieldValueChange}
               />
+              <InputField label='Last Name'
+                          name="lastName"
+                          type='text'
+                          value={lastName}
+                          message='*Required'
+                          error={false}
+                          handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <InputField label='Email Address'
+                          name="email"
+                          type='email'
+                          value={email}
+                          message='*Required'
+                          error={false}
+                          handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <InputField label='Password'
+                          name="password"
+                          type={showPassword ? 'text' : 'password'}
+                          value={password}
+                          message='*Required'
+                          error={false}
+                          addonIcon={showPassword ? "/assets/images/hidePassword.jpg" : "/assets/images/showPassword.jpg"}
+                          handleFieldValueChange={this.handleFieldValueChange}
+                          onAddonClick={this.onAddonClick}
+              />
+              <InputField label='Confirm Password'
+                          name="confirmPassword"
+                          type={showConfirmPassword ? 'text' : 'confirmPassword'}
+                          value={confirmPassword}
+                          message='*Required'
+                          error={false}
+                          addonIcon={showPassword ? "/assets/images/hidePassword.jpg" : "/assets/images/showPassword.jpg"}
+                          handleFieldValueChange={this.handleFieldValueChange}
+                          onAddonClick={this.onAddonClick}
+              />
+              <Col xl={12} lg={12} md={12} sm={12} xs={12} className="d-flex px-0">
+                <Button onClick={this.stepOneValidation} className="submitButton w-100">Next Step</Button>
+              </Col>
+            </Col>
+            }
+            {stepNumber === 2 &&
+            <Col xl={{ size: 4, offset: 4 }} className="justify-content-center d-flex flex-column align-items-center">
+              <img src="/assets/images/signUpSecondStepIcon.png" alt="Step 1"/>
+              <p className="signUpStepNumber">Step 2 of 2</p>
+              <h2 className="signUpHeader">Where are you from?</h2>
+              <SelectField label='Country'
+                           name="country"
+                           value={country}
+                           message='*Required'
+                           error={false}
+                           options={countryOptions}
+                           handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <InputField label='Zip Code'
+                          name="zip"
+                          type='text'
+                          value={zip}
+                          message='*Required'
+                          error={false}
+                          handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <InputField label='City'
+                          name="city"
+                          type='text'
+                          value={city}
+                          message='*Required'
+                          error={false}
+                          handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <InputField label='State'
+                          name="state"
+                          type='text'
+                          value={state}
+                          message='*Required'
+                          error={false}
+                          handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <InputField label='Address'
+                          name="address"
+                          type='text'
+                          value={address}
+                          message='*Required'
+                          error={false}
+                          handleFieldValueChange={this.handleFieldValueChange}
+              />
+              <Col xl={12} lg={12} md={12} sm={12} xs={12} className="d-flex px-0">
+                <Button onClick={this.stepTwoValidation} className="submitButton w-100">Sign Up</Button>
+              </Col>
             </Col>
             }
           </Col>
