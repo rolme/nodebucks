09ef92f8165df78bb51fc003a8bf9ddfef38304b
Ron Parnaso <roland.parnaso@gmail.com>
2018-06-20 08:32:12 -0700

    expose ROI.

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -7,28 +7,12 @@ class CryptosController < ApplicationController
 
   def show
     @crypto       = Crypto.find_by(slug: params[:slug])
-    @show_pricing = true
     @show_roi     = true
 
     # TODO: Figure out a way to store orders
     np      = NodeManager::Pricer.new
     @prices = np.evaluate(@crypto)
     @orders = np.orders
-  end
-
-  def update
-    @crypto = Crypto.find params[:slug]
-    if @crypto.update(crypto_params)
-      render 'show'
-    else
-      render json: { status: 'error', message: 'Could not update' }
-    end
-  end
-
-protected
-
-  def crypto_params
-    params.require(:crypto).permit(:price)
   end
 
 end
