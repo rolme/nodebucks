e323c6dd6ad2861a87bfa452e5f8ed111c252284
Ron Parnaso <roland.parnaso@gmail.com>
2018-06-30 11:32:47 -0700

    Add missing annual and last 30 days percentage.

--- client/src/containers/dashboard/summary.js
+++ client/src/containers/dashboard/summary.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import {Col} from 'reactstrap'
 
 export default class Summary extends Component {
   render() {
@@ -8,20 +7,25 @@ export default class Summary extends Component {
 
     if (list.length > 0) {
       summary = list.reduce((data, item) => {
-        data.rewardTotal = +data.rewardTotal + +item.rewardTotal
-        data.value   = +data.value + +item.value
-        data.cost    = +data.cost + +item.cost
-        data.rewards.month   = +data.rewards.month + +item.rewards.month
+        data.rewardTotal           = +data.rewardTotal + +item.rewardTotal
+        data.value                 = +data.value + +item.value
+        data.cost                  = +data.cost + +item.cost
+        data.rewards.month         = +data.rewards.month + +item.rewards.month
+        data.crypto.yearlyRoiValue = +data.crypto.yearlyRoiValue + +item.crypto.yearlyRoiValue
         return data
       })
     } else {
       return <span>Pending...</span>
     }
 
-    const rewardTotal = (+summary.rewardTotal).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-    const value       = (+summary.value).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-    const cost        = (+summary.cost).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-    const month       = (+summary.rewards.month).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    // TODO: Should have a method to fix to 2 and add commas
+    const rewardTotal     = (+summary.rewardTotal).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    const value           = (+summary.value).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    const cost            = (+summary.cost).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    const month           = (+summary.rewards.month).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    const annual          = ((+summary.crypto.yearlyRoiValue/+summary.cost) * 100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    const monthPercentage = ((+summary.rewards.month/+summary.cost) * 100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+
     return (
       <div className="card">
         <div className="card-header">
@@ -30,7 +34,7 @@ export default class Summary extends Component {
         <div className="card-body">
           <dl className="row">
             <dt className="col-sm-7">Reward Balance</dt>
-            <dd className="col-sm-5 text-right">{rewardTotal}</dd>
+            <dd className="col-sm-5 text-right">$ {rewardTotal}</dd>
 
             <dt className="col-sm-7">Total Node(s) Value</dt>
             <dd className="col-sm-5 text-right">{value}</dd>
@@ -39,10 +43,10 @@ export default class Summary extends Component {
             <dd className="col-sm-5 text-right">{cost}</dd>
 
             <dt className="col-sm-7">Projected Annual %</dt>
-            <dd className="col-sm-5 text-right">?</dd>
+            <dd className="col-sm-5 text-right">{annual} %</dd>
 
             <dt className="col-sm-7">Last 30 days</dt>
-            <dd className="col-sm-5 text-right">{month}</dd>
+            <dd className="col-sm-5 text-right">{month} ({monthPercentage} %)</dd>
           </dl>
         </div>
       </div>
