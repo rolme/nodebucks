072d02f5cbc5befade1930ab754fccb02e4887ef
Roland Parnaso <Roland Parnaso>
2018-07-12 13:45:27 -0400

    Properly calculate ROI based on todays coin price.

--- app/services/crypto_scraper.rb
+++ app/services/crypto_scraper.rb
@@ -6,25 +6,26 @@ class CryptoScraper
 
   def self.scrape(a_crypto=nil)
     cryptos = (a_crypto.present?) ? [a_crypto] : Crypto.active
-
     if Rails.env != 'development'
-      self.server_scrape(cryptos)
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.binary = ENV['GOOGLE_CHROME_SHIM']
+      options.add_argument('--headless')
+      browser = Selenium::WebDriver.for :chrome, options: options
     else
-      self.local_scrape(cryptos)
+      driver = Watir::Browser.new
     end
-  end
 
-  def self.server_scrape(cryptos)
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.binary = ENV['GOOGLE_CHROME_SHIM']
-    options.add_argument('--headless')
-
-    browser = Selenium::WebDriver.for :chrome, options: options
     cryptos.each do |crypto|
       path = "https://masternodes.pro/stats/#{crypto.symbol}/statistics"
-      begin
+      if Rails.env != 'development'
         browser.navigate.to path
-        sleep 2
+      else
+        driver.goto path
+        browser = driver.wd
+      end
+      sleep 1
+
+      begin
         crypto.daily_reward         = browser.find_elements(tag_name: 'mnp-data-box')[7].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
         crypto.estimated_node_price = browser.find_elements(tag_name: 'mnp-data-box')[2].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
         crypto.masternodes          = browser.find_elements(tag_name: 'mnp-data-box')[3].text&.split(/\n/).first.gsub(/\D/,'').to_i
@@ -33,31 +34,6 @@ class CryptoScraper
 
         el = browser.find_elements(tag_name: 'a')&.find{ |a| a.attribute('title') == 'WebSite' }
         crypto.url = el.attribute('href').split("r=")[1] if el.present?
-
-        crypto.save
-      rescue => error
-        Rails.logger.error "SCRAPE ERROR: #{error}"
-        Rails.logger.error "SCRAPE ERROR PATH: #{path}"
-      end
-    end
-    browser.close
-  end
-
-  def self.local_scrape(cryptos)
-    browser = Watir::Browser.new
-    cryptos.each do |crypto|
-      path = "https://masternodes.pro/stats/#{crypto.symbol}/statistics"
-      begin
-        browser.goto path
-        sleep 1
-        crypto.daily_reward         = browser.wd.find_elements(tag_name: 'mnp-data-box')[7].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
-        crypto.estimated_node_price = browser.wd.find_elements(tag_name: 'mnp-data-box')[2].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
-        crypto.masternodes          = browser.wd.find_elements(tag_name: 'mnp-data-box')[3].text&.split(/\n/).first.gsub(/\D/,'').to_i
-        crypto.price                = browser.wd.find_elements(tag_name: 'mnp-data-box')[6].text&.split(/\n/).first.gsub(/[^\d\.]/, '').to_f
-        crypto.stake                = browser.wd.find_elements(tag_name: 'mnp-data-box')[4].text&.split(/\s/).first.gsub(/\D/,'').to_i
-
-        el = browser.a(title: 'WebSite')
-        crypto.url = el.href.split("r=")[1] if el.exists?
 
         crypto.save
       rescue => error
