7c842efb0999f040c78925a94f378ce5f57084cb
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-07-25 18:39:19 +0400

    Added FAQ link to footer

--- client/src/containers/app/footer/index.js
+++ client/src/containers/app/footer/index.js
@@ -12,7 +12,8 @@ class Footer extends Component {
             <NavLink to='/' className="footerText"><img src="/assets/images/shortLogo.png" alt="logo"/></NavLink>
             <p className="footerText pl-2"> All Rights Reserved © 2018</p>
           </Col>
-          <Col xl={{ size: 4, offset: 1 }} lg={{ size: 5, offset: 1 }} md={{ size: 7, offset: 0 }} sm={{ size: 12, offset: 0 }} xs={{ size: 12, offset: 0 }} className="d-flex px-0 ">
+          <Col xl={{ size: 5, offset: 1 }} lg={{ size: 6, offset: 1 }} md={{ size: 7, offset: 0 }} sm={{ size: 12, offset: 0 }} xs={{ size: 12, offset: 0 }} className="d-flex px-0 ">
+            <NavLink to='/faq' className="footerText pr-4">FAQ</NavLink>
             <NavLink to='/terms' className="footerText pr-4">Terms of Use</NavLink>
             <a href="https://nodebucks.freshdesk.com" target="_new" className="footerText pr-4">Support</a>
             <NavLink to='/privacy' className="footerText pr-4">Privacy Policy</NavLink>
