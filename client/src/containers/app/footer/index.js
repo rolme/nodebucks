1e9d7ef03b9e45eac459b1b7b0b58eaf52cdf852
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-07-27 15:51:50 +0400

    Prevent footer links from wrapping

--- client/src/containers/app/footer/index.js
+++ client/src/containers/app/footer/index.js
@@ -12,7 +12,7 @@ class Footer extends Component {
             <NavLink to='/' className="footerText"><img src="/assets/images/shortLogo.png" alt="logo"/></NavLink>
             <p className="footerText pl-2"> All Rights Reserved © 2018</p>
           </Col>
-          <Col xl={{ size: 5, offset: 1 }} lg={{ size: 6, offset: 1 }} md={{ size: 7, offset: 0 }} sm={{ size: 12, offset: 0 }} xs={{ size: 12, offset: 0 }} className="d-flex px-0 ">
+          <Col xl={{ size: 5, offset: 1 }} lg={{ size: 6, offset: 1 }} md={{ size: 7, offset: 0 }} sm={{ size: 12, offset: 0 }} xs={{ size: 12, offset: 0 }} className="d-flex px-0 flex-wrap">
             <NavLink to='/faq' className="footerText pr-4">FAQ</NavLink>
             <NavLink to='/terms' className="footerText pr-4">Terms of Use</NavLink>
             <a href="https://nodebucks.freshdesk.com" target="_new" className="footerText pr-4">Support</a>
