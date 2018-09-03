0e46cb76fd4d474eeb4e80b128401208d5696568
Ron Parnaso <roland.parnaso@gmail.com>
2018-09-03 11:31:01 -0700

    hotfix - allow user to see their own orders.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,13 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_admin_request, only: [:index]
+  before_action :authenticate_request, only: [:index]
 
   def index
-    @orders = Order.includes(:node, :user)
-                .filter_by_node(params[:n])
-                .filter_by_user(params[:u])
+    if current_user.admin?
+      @orders = Order.includes(:node, :user)
+                  .filter_by_node(params[:n])
+                  .filter_by_user(params[:u])
+    else
+      @orders = Order.includes(:node, :user).where(user_id: current_user.id)
+    end
   end
 end
