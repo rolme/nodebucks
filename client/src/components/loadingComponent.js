a29272cebc86f135d7e864086d51f43dbb893661
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-09-17 11:24:02 +0400

    Try to fix site stop working issue

--- client/src/components/loadingComponent.js
+++ client/src/components/loadingComponent.js
@@ -12,7 +12,7 @@ export default function Loading(props) {
     }
   } else if (props.error) {
     !!props.retry && typeof props.retry === "function" &&  props.retry();
-    console.log('ERROR:', props)
+    console.log('LOADING ERROR:', props)
     return <div>Error! Component failed to load</div>;
   } else {
     return null;
