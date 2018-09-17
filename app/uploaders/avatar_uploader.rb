5180af2835586657cfaa3a2ceac54e0d937ae0ff
Ron Parnaso <roland.parnaso@gmail.com>
2018-09-17 08:48:07 -0700

    hotfix upload filename

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,4 +2,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "avatars"
   end
+
+  def filename
+    "#{model.id}.#{file.extension}" if original_filename.present?
+  end
 end
