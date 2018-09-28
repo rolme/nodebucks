6225b7f274c8a42bda2b5a0999c3d6e1927dae02
Aleksandar M <salac@kolosek.com>
2018-09-28 16:24:54 +0200

    Sending notification if node server status is not online or not responding

---
+++ app/workers/node_offline_notifier_worker.rb
@@ -0,0 +1,15 @@
+class NodeOfflineNotifierWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false, backtrace: true
+
+  def perform(*args)
+    Node.all.each do |node|
+      if node.status != 'online'
+        SupportMailerService.send_node_not_online_notification(node)
+      elsif node.ip.present? && !Utils.server_up?(node.ip)
+        node.update_attribute(:status, :offline)
+        SupportMailerService.send_node_offline_notification(node)
+      end
+    end
+  end
+end
