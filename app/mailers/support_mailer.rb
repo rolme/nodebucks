72af27b918d488af6674ceff91175c8a1db092b1
Ron Parnaso <roland.parnaso@gmail.com>
2018-10-06 09:53:00 -0700

    Send staging emails to Ron.

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -2,11 +2,19 @@ class SupportMailer < ApplicationMailer
   def send_email(subject, message)
     @subject = subject
     @message = message
-    mail(
-      content_type: "text/html",
-      subject: subject,
-      to: ['support@nodebucks.com'],
-      bcc: ['ron.parnaso@gmail.com', 'jay.severson@gmail.com']
-    )
+    if Rails.env.production?
+      mail(
+        content_type: "text/html",
+        subject: subject,
+        to: ['support@nodebucks.com'],
+        bcc: ['ron.parnaso@gmail.com', 'jay.severson@gmail.com']
+      )
+    elsif Rails.env.staging?
+      mail(
+        content_type: "text/html",
+        subject: subject,
+        to: ['roland.parnaso@gmail.com']
+      )
+    end
   end
 end
