48cf12b3cf2759417afc07415c4adba222c938ee
Roland Parnaso <Roland Parnaso>
2018-10-06 14:48:17 -0700

    email template

--- app/mailers/node_owner_mailer.rb
+++ app/mailers/node_owner_mailer.rb
@@ -1,18 +1,22 @@
 class NodeOwnerMailer < ApplicationMailer
-  def online(user, node)
-    @user = user
+  def online(node)
     @node = node
+    @user = node.user
 
-    mail(
-      :content_type => "text/html",
-      :subject => "Your #{node.name.capitalize} masternode is online.",
-      :to => @user.email
-    )
+    if @node.online_mail_sent_at.blank?
+      mail(
+        :content_type => "text/html",
+        :subject => "Your #{node.name.capitalize} masternode is online.",
+        :to => @user.email
+      )
+      @node.update_attribute(:online_mail_sent_at, DateTime.current)
+    end
   end
 
-  def reward(user, reward)
-    @user = user
+  def reward(reward)
     @reward = reward
+    @user   = reward.node.user
+    @account_amount = @user.balances.find { |b| b.name == @reward.node.name }.value
 
     mail(
       :content_type => "text/html",
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_234651) do
+ActiveRecord::Schema.define(version: 2018_10_06_211234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_234651) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.datetime "online_mail_sent_at"
     t.index ["account_id"], name: "index_nodes_on_account_id"
     t.index ["crypto_id"], name: "index_nodes_on_crypto_id"
     t.index ["slug"], name: "index_nodes_on_slug"
