ddc7863acfe7b5afbb71a0065c3f4b9e2e0ff064
Roland Parnaso <Roland Parnaso>
2018-10-10 17:50:29 -0700

    update version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 # The commit version is automatically overwritten by pre-commit hooks
-COMMIT_VERSION = 'be1be39e4d27a5c274084a0218f7d7e7f532e9fa'
+COMMIT_VERSION = '4250e33c62854a906c1b2a47f0e810010d8430fc'
 VERSION = '1.0.0'
