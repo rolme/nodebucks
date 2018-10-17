83baed2c993ff1b015a50e3fbcd35cbee500d5fd
Aleksandar M <salac@kolosek.com>
2018-10-17 12:05:15 +0200

    Added google sign in button

--- client/src/containers/authenticate/login.js
+++ client/src/containers/authenticate/login.js
@@ -190,7 +190,22 @@ class LogIn extends Component {
             <Col xl={{ size: 4 }} lg={{ size: 6 }} md={{ size: 6 }} className="justify-content-center d-flex flex-column align-items-center">
               {!isOnlyForm && <NavLink to="/"><img src="/assets/images/nodebucks_beta.svg" width="180" alt="sign in"/></NavLink>}
               {!isOnlyForm && <h2 className="logInHeader pageTitle">Welcome to Nodebucks</h2>}
-              <SocialButton provider='facebook' appId={process.env.REACT_APP_FACEBOOK_API_KEY} onLoginSuccess={this.handleSocialLogin.bind(this, 'facebook')} onLoginFailure={this.handleSocialLoginFailure.bind(this, 'facebook')} className="facebookSocialButton socialLogInButton"><i className="socialButtonIcon">&#xe809;</i> Sign in via facebook</SocialButton>
+              <SocialButton 
+                provider='facebook' 
+                appId={process.env.REACT_APP_FACEBOOK_API_KEY} 
+                onLoginSuccess={this.handleSocialLogin.bind(this, 'facebook')} 
+                onLoginFailure={this.handleSocialLoginFailure.bind(this, 'facebook')} 
+                className="socialButton facebookSocialButton">
+                  <i className="socialButtonIcon">&#xe809;</i> Sign in via Facebook
+              </SocialButton>
+              <SocialButton 
+                provider='google' 
+                appId={process.env.REACT_APP_GOOGLE_API_KEY} 
+                onLoginSuccess={this.handleSocialLogin.bind(this, 'google')} 
+                onLoginFailure={this.handleSocialLoginFailure.bind(this, 'google')} 
+                className="socialButton googleSocialButton">
+                  <i className="socialButtonIcon">&#xe805;</i> Sign in via Google
+              </SocialButton>
               <Col xl={{ size: 6, offset: 3 }} lg={{ size: 6, offset: 3 }} md={{ size: 6, offset: 3 }} sm={{ size: 6, offset: 3 }} xs={{ size: 6, offset: 3 }} className="dividerWithText">
                 <span>OR</span>
               </Col>
